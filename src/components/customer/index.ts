6bfaea1cc204ef71e9b50fa686fce4bd4e99539a
Lizbeth Selena Aldana Tarifeño <lsat-1699@hotmail.com>
2023-02-22 00:02:06 -0500

    fix: Modificando y borrando comentarios

--- src/components/customer/index.ts
+++ src/components/customer/index.ts
@@ -9,15 +9,4 @@ customerRouter.get("/:idCustomer", Controller.getOne);
 customerRouter.put("/:idCustomer", Controller.update);
 customerRouter.delete("/:idCustomer", Controller.remove);
 
-// customerRouter.post("/storeAddress/:idCustomer", Controller.storeAddress);
-
 export default customerRouter;
-
-// const addressRouter: Router = Router();
-
-// addressRouter.get("/", Controller.findAllAddresses);
-// addressRouter.get("/:idCustomerAddress", Controller.getOneAddress);
-// addressRouter.put("/:idCustomerAddress", Controller.updateAddress);
-// addressRouter.delete("/:idCustomerAddress", Controller.removeAddress);
-
-// export default addressRouter;
--- src/components/index.ts
+++ src/components/index.ts
@@ -2,6 +2,5 @@ export { default as CategoryRouter } from "./category";
 export { default as ProductRouter } from "./product";
 export { default as DiscountRouter } from "./discount";
 export { default as InventoryRouter } from "./inventory";
-
 export { default as CustomerRouter } from "./customer"
 export { default as PaymentRouter } from "./payment"
