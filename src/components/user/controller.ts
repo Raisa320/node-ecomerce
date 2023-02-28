697b8b0a0a1ca31711b3ebdf02e52b8e9adc55bb
Juan  Limo <pachuca.sanpedro@gmail.com>
2023-02-27 21:52:23 -0500

    Encriptación de contraseña

--- src/components/user/controller.ts
+++ src/components/user/controller.ts
@@ -1,15 +1,22 @@
 import type { Request, Response } from "express";
 import prisma from "../../datasource";
 import { failure, success } from "../../response";
-import { encriptPass } from "../../utils/controller";
+import { encriptPass } from "../../utils/index";
 
 
 export const store = async (req: Request, res: Response): Promise<Response> => {
   try {
     const data = req.body;
-    const hashedPassword = encriptPass(data);
+    const hashedPassword = await encriptPass(data.password);
     const user = await prisma.user.create({ 
-      data: data ,
+      data: {
+        username: data.username,
+        password: String(hashedPassword),
+        first_name: data.first_name,
+        last_name: data.last_name,
+        telephone: data.telephone,
+        rol_id: data.rol_id 
+      } ,
       include:{rol:true}
     
     });
@@ -21,6 +28,7 @@ export const store = async (req: Request, res: Response): Promise<Response> => {
       data: user,
     });
   } catch (error: any) {
+    console.log(error)
     return failure({
       res,
       message: error,
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,62 +1,10 @@
-// import { Router } from "express";
-// import * as Controller from "./controller";
 
-// const userRouter: Router = Router();
-
-// userRouter.post("/", Controller.store);
+import bcrypt from "bcrypt";
 
 
-// export default userRouter;
+export const encriptPass = (data:any) => {
 
+    let hashedPassword = bcrypt.hash(data, 8);
+    return hashedPassword;
+}
 
-
-
-
-
-// import bcrypt from "bcrypt";
-// import type { Request, Response } from "express";
-// import express  from "express"; 
-// import prisma from "../datasource";
-// const index = express();
-
-// index.use(express.urlencoded({extended:false}));
-// index.use(express.json())
-// index.post('/login',async (req:Request, res:Response)=>{
-// const username = req.body.username;
-// const password = req.body.password;
-// const foundUsername = await prisma.user.findUnique({
-//     where:{
-//         username: username
-//     }
-// })
-// const foundPassword = await prisma.user.findFirst({
-//     where:{
-//         password: password
-//     }
-// })
-// if(username== foundUsername && password == password)
-// {
-//     let hashedPassword =await bcrypt.hash(password, 8);
-//     res.json({
-//         message:'VERDADERO',
-//         hashedPassword : hashedPassword
-//     })
-// }else {
-//     res.json({
-//         message: 'FALSO'
-//     })
-// }
-
-// })
-
-// index.listen(8000,()=>{
-//     console.log('open')
-// })
-
-
-// const enteredPassword = 'mypassword';
-// if (bcrypt.compareSync(enteredPassword, hashedPassword)) {
-//     console.log('La contraseña es correcta');
-// } else {
-//     console.log('La contraseña es incorrecta');
-// }
